7845860f48209164d8b49e7cf7e6744cf4cc538e
[AI7] 민준영 <znddiqjwjs@gmail.com>
2023-06-02 22:56:09 +0900

    Fix : 오류 수정

---
+++ front/main.jsx
@@ -0,0 +1,13 @@
+import './style.css';
+
+// import { setupCounter } from './counter.js';
+import App from './App';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.querySelector('#app'),
+);
---
+++ front/vite.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    build: {
+        // esbuild 로더 설정 추가
+        loader: {
+            '.js': 'jsx',
+        },
+    },
+};
