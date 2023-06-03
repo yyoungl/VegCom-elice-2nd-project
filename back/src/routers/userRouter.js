509c83107e73b89f5f6500829e9d66e1939bab8d
[AI7] 민준영 <znddiqjwjs@gmail.com>
2023-06-03 23:04:21 +0900

    Style : 줄맞춤

--- back/src/routers/userRouter.js
+++ back/src/routers/userRouter.js
@@ -4,21 +4,19 @@ import { userAuthService } from '../services/userService.js';
 
 const userAuthRouter = Router();
 
-userAuthRouter.post("/register", async function (req, res, next) {
+userAuthRouter.post('/register', async function (req, res, next) {
     try {
-        const {email, password, nickname} = req.body;
-        const isDuplicate = await userAuthService.checkDuplicate({email});
+        const { email, password, nickname } = req.body;
+        const isDuplicate = await userAuthService.checkDuplicate({ email });
 
         if (isDuplicate) {
-            res.status(409).send({ message: "이미 존재하는 이메일 입니다." });
+            res.status(409).send({ message: '이미 존재하는 이메일 입니다.' });
             throw new Error(user.errorMessage);
-        } 
+        }
 
-          const user = await userAuthService.createUser({ email, password, nickname});
-          res.status(200).send({user});
-
-        
-    }catch (error) {
+        const user = await userAuthService.createUser({ email, password, nickname });
+        res.status(200).send({ user });
+    } catch (error) {
         next(error);
     }
 });
@@ -57,7 +55,3 @@ userAuthRouter.get('/isLogin', login_required, async function (req, res, next) {
 });
 
 export { userAuthRouter };
-<<<<<<< HEAD
-=======
-
->>>>>>> 835735056ba260cb311e20222a0141ee4ffa74db
