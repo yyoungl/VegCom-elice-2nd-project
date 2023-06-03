d479e9fe3bf53c8640c628cb64c101b3762f195e
[AI7] 민준영 <znddiqjwjs@gmail.com>
2023-06-03 22:28:02 +0900

    Comment : login_required.js 주석 추가

--- back/src/middlewares/login_required.js
+++ back/src/middlewares/login_required.js
@@ -2,13 +2,15 @@ import passport from 'passport';
 import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
 
 const jwtOptions = {
+    // JWT 토큰의 비밀 키 설정
     secretOrKey: process.env.JWT_SECRET_KEY || 'jwt-secret-key',
+    // 토큰 추출 방식 설정
     jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 };
 
 passport.use(
     new JwtStrategy(jwtOptions, (jwtPayload, done) => {
-        // JWT 토큰으로부터 추출한 userId를 검증하고, 검증 결과를 done 콜백 함수를 통해 전달합니다.
+        // JWT 토큰으로부터 추출한 userId를 검증하고, 검증 결과를 done 콜백 함수를 통해 전달
         const userId = jwtPayload.userId;
         if (userId) {
             return done(null, userId);
