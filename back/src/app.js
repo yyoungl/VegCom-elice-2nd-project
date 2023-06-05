35ca14f81d3384644029ac4ff41e8a3ada140b32
[AI7] 민준영 <znddiqjwjs@gmail.com>
2023-06-05 21:53:48 +0900

    Feat : 랭킹 리스트 불러오기 기능 추가

--- back/src/app.js
+++ back/src/app.js
@@ -2,9 +2,10 @@ import cors from 'cors';
 import express from 'express';
 
 import { errorMiddleware } from './middlewares/errorMiddleware.js';
-// import { login_required } from './middlewares/login_required.js';
+import { login_required } from './middlewares/login_required.js';
 
 import { userAuthRouter } from './routers/userRouter.js';
+import { rankRouter } from './routers/rankRouter.js';
 
 const app = express();
 
@@ -24,6 +25,7 @@ app.get('/', (req, res) => {
 
 // router, service 구현 (userAuthRouter는 맨 위에 있어야 함.)
 app.use('/user', userAuthRouter);
+app.use('/rank', rankRouter);
 
 // 순서 중요 (router 에서 next() 시 아래의 에러 핸들링  middleware로 전달됨)
 app.use(errorMiddleware);
