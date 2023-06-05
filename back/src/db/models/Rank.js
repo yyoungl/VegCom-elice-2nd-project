694608d89fbcc63b7a17e6298bf52021bf2bb7a7
[AI7] 민준영 <znddiqjwjs@gmail.com>
2023-06-06 00:46:12 +0900

    Fix : 랭킹 정보 불러오기 중 모든 정보를 출력할 수 있게 수정

--- back/src/db/models/Rank.js
+++ back/src/db/models/Rank.js
@@ -6,7 +6,7 @@ class Rank {
             'SELECT user.id as userId, user.nickname, user.userImage, point.accuPoint, (SELECT count(*) FROM post WHERE post.userId = user.id) as storyCount FROM user JOIN point ON user.id = point.userId ORDER BY point.accuPoint desc';
         const [rows] = await mysqlDB.query(query);
 
-        return rows[0];
+        return rows;
     }
 }
 
